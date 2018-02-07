41a146478fb73ac742a4f9b0087531586679eba2
Scott Cinatl <scottcinatl@gmail.com>
2018-02-07 11:09:28 -0800

    link to source code added

--- src/components/layoutComponents/Footer.js
+++ src/components/layoutComponents/Footer.js
@@ -9,7 +9,9 @@ class Footer extends Component {
         <a href="#search-form">
           <h3>Back To Search</h3>
         </a>
-        <h2>View Source Code</h2>
+        <a href="https://github.com/scottcinatl/marvel-data-adventures" target="_blank" rel="noopener noreferrer">
+          <h2>View Source Code</h2>
+        </a>
         <a href="https://developer.marvel.com/" target="_blank" rel="noopener noreferrer">
           <h1>Data provided by Marvel. © 2014 Marvel</h1>
         </a>
